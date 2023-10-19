799454baae97c63df278ca7d45e7119a4706be4b
sandeep <sands749@gmail.com>
2023-10-19 14:39:25 -0500

    updated s3 files

---
+++ S3 Bucket/variables.tf
@@ -0,0 +1,19 @@
+variable "bucket_name" {
+    type = string 
+    default = "my-bucket terraform010101"
+}
+
+variable "access_key" {
+    type = string
+    default = "*****"
+}
+
+variable "secret_key" {
+    type = string
+    default= "*******"
+}
+
+variable "region" {
+    type = string
+    default = "us-east-2"
+}
